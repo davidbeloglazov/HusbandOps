bfa552a4a80c4aea28ab1d5ac64aa798d514b00e
Chris Beloglazov <vb9129@princeton.edu>
2025-10-29 12:18:41 -0400

    Initial Commit

---
+++ Mission Husband/Mission_HusbandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mission_HusbandApp.swift
+//  Mission Husband
+//
+//  Created by David Beloglazov on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Mission_HusbandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
